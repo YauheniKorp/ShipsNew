2a4059daa654f1b7a33259c81ef280df112c7fb0
Yauheni Korp <ayonamirei1@gmail.com>
2020-10-21 11:18:47 +0300

    no message

---
+++ Ships/BattleViewController.swift
@@ -0,0 +1,30 @@
+//
+//  BattleViewController.swift
+//  Ships
+//
+//  Created by Admin on 21.10.2020.
+//  Copyright © 2020 Admin. All rights reserved.
+//
+
+import UIKit
+
+class BattleViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
